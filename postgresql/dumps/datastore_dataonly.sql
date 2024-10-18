5548ef10385e5d776229091353f2599ea0a038b0
ddecampos <djosedecampos@buenosaires.gob.ar>
2024-10-18 14:00:26 -0300

    Se agregan dumps dataonly

---
+++ postgresql/dumps/datastore_dataonly.sql
@@ -0,0 +1,48 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.15
+-- Dumped by pg_dump version 13.16
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: 3d3e45dd-2367-4699-a4a5-55abe3bb0afa; Type: TABLE DATA; Schema: public; Owner: datastore_ro
+--
+
+
+
+--
+-- Data for Name: c7604eec-c599-4dbc-8c4d-472239533051; Type: TABLE DATA; Schema: public; Owner: datastore_ro
+--
+
+
+
+--
+-- Name: 3d3e45dd-2367-4699-a4a5-55abe3bb0afa__id_seq; Type: SEQUENCE SET; Schema: public; Owner: datastore_ro
+--
+
+SELECT pg_catalog.setval('public."3d3e45dd-2367-4699-a4a5-55abe3bb0afa__id_seq"', 1, false);
+
+
+--
+-- Name: c7604eec-c599-4dbc-8c4d-472239533051__id_seq; Type: SEQUENCE SET; Schema: public; Owner: datastore_ro
+--
+
+SELECT pg_catalog.setval('public."c7604eec-c599-4dbc-8c4d-472239533051__id_seq"', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
